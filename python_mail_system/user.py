019b652dae429e10c9ea8f1da31f1d6c23651cc4
PandAJ2021 <pandaj200021@gmail.com>
2023-04-22 15:15:14 +0330

    password validate and hashing added

--- python_mail_system/user.py
+++ python_mail_system/user.py
@@ -1,32 +1,50 @@
 import re
+import secrets
+import hashlib
 from exceptions import *
+
 
 class User:
     users = []
-    def __init__(self , full_name , username , password):
+
+    def __init__(self, full_name: str, username: str, password: str):
         self.full_name = full_name
         self.username = username
         self.password = password
+        self.salt = secrets.token_hex(15)
+        __class__.users.append(self)
 
     @property
     def full_name(self):
         return self._full_name
-    
+
     @full_name.setter
-    def full_name(self , name):
-        #pattern: start by bounch of letters and pass with 1-3 group of space+letters
+    def full_name(self, name):
+        # pattern: start by bounch of letters and pass with 1-3 group of space+letters
         if not re.match(r'^[a-zA-Z]+( [a-zA-Z]+){1,5}$', name):
             raise InvalidNameFormat()
-        self._full_name = name 
+        self._full_name = name
 
     @property
     def username(self):
-        return self._username 
-    
+        return self._username
+
     @username.setter
-    def username(self , value):
+    def username(self, value):
         if not re.match(r'^(?=.*[A-Z])(?=.*\d)(?=.*[_\-.])[a-zA-Z0-9_\-.]+$', value):
             raise InvalidUsername()
         self._username = value
 
-# test = User('ali jalili', 'Ali-jalili', 'password')
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        if not re.match(r'^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$', value):
+            raise InvalidPassword()
+        passwd = value + self.salt
+        hashed = hashlib.md5(passwd.encode())
+        self._password = hashed
+
