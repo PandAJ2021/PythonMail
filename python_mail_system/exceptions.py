ebdd6425ec1682c32d50b08443928838efe30cbd
PandAJ2021 <pandaj200021@gmail.com>
2023-04-22 13:43:18 +0330

    InvalidNameFormat and InvalidUsername format classes add in exceptions.py

---
+++ python_mail_system/exceptions.py
@@ -0,0 +1,16 @@
+class AuthException(Exception):
+    pass
+
+
+class InvalidNameFormat(AuthException):
+    def __init__(self):
+        super().__init__('Invalid format: your name does not match the required format.' +
+                         'Please enter a valid \nname with two or more sections,' +
+                         'each separated by a single space and containing only letters.')
+
+
+class InvalidUsername(AuthException):
+    def __init__(self):
+        super().__init__("Invalid username format: usernames must contain at least one"+
+            " uppercase letter, one\n digit, and one of the characters '_', '-', or '.' ")
+
