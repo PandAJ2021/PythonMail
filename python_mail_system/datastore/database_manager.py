a48bf966891415b8cecbca1b2681a8fec938fe36
PandAJ2021 <pandaj200021@gmail.com>
2023-04-22 19:27:44 +0330

    DatabaseHandler added

--- python_mail_system/datastore/database_manager.py
+++ python_mail_system/datastore/database_manager.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+
 class PostgreSQLDatabase:
     def __init__(self, dbname, user, password, host='localhost', port=5432):
         self.dbname = dbname
@@ -27,11 +28,73 @@ class PostgreSQLDatabase:
     def execute(self, query, *params):
         with self.conn.cursor() as cur:
             cur.execute(query, params)
-            return cur.fetchall()
+            if cur.description is not None:  # handle a cursor that has not executed a SELECT statement
+                return cur.fetchall()
 
 
-test = PostgreSQLDatabase('postgres' , 'postgres' , '1234')
-test.connect()
-print(test.execute(''''''))
+class DatabaseHandler:
+
+    @staticmethod
+    def create_table(table_name: str, data: dict, database=PostgreSQLDatabase):
+        query = f'CREATE TABLE IF NOT EXISTS {table_name} ('
+        query += ','.join([f'{col} {data[col]}' for col in data])
+        query += ');'
+
+        database.connect()
+        database.execute(query)
+        database.close()
+
+    @staticmethod
+    def insert(table_name: str, data: dict, ddatabase=PostgreSQLDatabase):
+        columns = ','.join(data.keys()) 
+        values = ','.join(data.values()) 
+        query = f"INSERT INTO {table_name}" + \
+            f"({columns})" + f'\n VALUES({values});'
+
+        database.connect()
+        database.execute(query)
+        database.close()
+
+    @staticmethod
+    def read(table_name: str, condition=None, database=PostgreSQLDatabase):
+        query = f"SELECT * FROM {table_name}"
+        if condition:
+            query += f' WHERE {condition}'
+        query += ';'
+
+        database.connect()
+        return database.execute(query)
+
+    @staticmethod
+    def update(data: dict, table_name: str, condition: str, database=PostgreSQLDatabase):
+
+        query = f'UPDATE {table_name} SET '
+        query += ', '.join([f"{col}=%s" for col in data])
+        query += ') '
+        query += f' WHERE {condition} ;'
+
+        database.connect()
+        database.execute(query)
+        database.close()
+
+    @staticmethod
+    def delete(condition: str, database=PostgreSQLDatabase):
+        query = f'DELETE FROM {table_name} WHERE {condition}'
+
+        database.connect()
+        database.execute(query)
+        database.close()
 
 
+test = PostgreSQLDatabase('postgres', 'postgres', '1234')
+data = {
+    'name': 'varchar(80)',
+    'age': 'int'
+}
+DatabaseHandler.create_table('test' , data , test)
+# print(DatabaseHandler.read('test' ,database=test))
+insert_data = {
+
+
+}
+DatabaseHandler.insert('test', data)
