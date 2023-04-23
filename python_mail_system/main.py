4bc8540e786d5c539e3a6ba264eff5b5a2f01f9f
PandAJ2021 <pandaj200021@gmail.com>
2023-04-23 16:40:36 +0330

    Update user manager and model and ... pycache files

--- python_mail_system/main.py
+++ python_mail_system/main.py
@@ -4,4 +4,9 @@ from user.user_manager import UserManager
 
 
 
-UserManager.register_user('ALI JALILI', 'Aj235757j-i', 'Al12345678')
+UserManager.register_user('mohammad jalili', 'MJ25757j-i', 'Mj12345678')
+# a = UserManager.authentication('mohammad jalili', 'Mj235757j-i', 'Mj12345678')
+# print(a)
+
+# a= UserManager.auth_pass('MJ25757j-i', 'Mj2345678')
+# print(a)
