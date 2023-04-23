a37a15c3a9343a687c03a8cb296f5ce188bf9c4f
PandAJ2021 <pandaj200021@gmail.com>
2023-04-23 12:08:52 +0330

    import modules in main

---
+++ python_mail_system/main.py
@@ -0,0 +1,7 @@
+from datastore.database_manager import DatabaseHandler
+from utils.database_connector import PostgreSQLDatabase
+from user.user_manager import UserManager
+
+
+
+UserManager.register_user('ALI JALILI', 'Aj235757j-i', 'Al12345678')
