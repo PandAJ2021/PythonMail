2444d76ea6c928859d357fc3e75f83d11b262ffa
PandAJ2021 <pandaj200021@gmail.com>
2023-04-23 21:29:31 +0330

    some pycach files auto updated

--- python_mail_system/main.py
+++ python_mail_system/main.py
@@ -4,9 +4,13 @@ from user.user_manager import UserManager
 
 
 
-UserManager.register_user('mohammad jalili', 'MJ25757j-i', 'Mj12345678')
+# UserManager.register_user('mohammad jalili', 'MJ25757j-i', 'Mj12345678')
+# UserManager.register_user('ali jalili', 'AJ25757j-i', 'AJ12345678' ,True)
 # a = UserManager.authentication('mohammad jalili', 'Mj235757j-i', 'Mj12345678')
 # print(a)
 
 # a= UserManager.auth_pass('MJ25757j-i', 'Mj2345678')
 # print(a)
+a = UserManager.logging_user('MJ25757j-i', 'Mj12345678')
+
+print(a.id)
