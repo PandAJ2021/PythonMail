7feea468c41a80f7b3398ad816902b248fbd41e5
PandAJ2021 <pandaj200021@gmail.com>
2023-04-23 12:13:02 +0330

    some changed in user_manager file: UserManager corrected and register_user methode completed

--- python_mail_system/user/user_manager.py
+++ python_mail_system/user/user_manager.py
@@ -1,18 +1,35 @@
 from user.user_model import User
+from datastore.database_manager import DatabaseHandler
+from utils.database_connector import PostgreSQLDatabase
+from exceptions import *
+
+
+user_db_connector = PostgreSQLDatabase(
+    dbname='postgres', user='postgres', password='1234')
+
+users_db = DatabaseHandler('users', user_db_connector)
+
+users_db.create_table({
+    'id': 'bigserial primary key Not Null',
+    'name': 'varchar(50)',
+    'username':'varchar(50)',
+    'salt': 'text Not Null',
+    'password': 'text Not null'
+})
+
 
 class UserManager:
 
-    def __init__(self , user = User):
-        self.user = user    
-        self.register = False
-
-    def register_user(self,full_name: str, username: str, password: str ):
-        
+    @staticmethod
+    def register_user(full_name: str, username: str, password: str):
         try:
-            instance = self.user(full_name , username , password)
-        except Exception as err:
+            user = User(full_name, username, password)
+            if not users_db.read(f"username = '{username}' and name = '{full_name.lower()}'"):
+                users_db.insert(user.return_dict())
+            else:
+                raise UserAlreadyExist()
+                
+        except (InvalidNameFormat , InvalidPassword , InvalidUsername) as err:
             print(err)
-
-
-test= User('Ali Jalili', 'Li23582_d', '12345aaL')
-print(test.__dict__)
+            
+        
