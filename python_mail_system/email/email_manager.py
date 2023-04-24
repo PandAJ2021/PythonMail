532ee01703e7fd5367cb5409b400eb2a31369ed5
PandAJ2021 <pandaj200021@gmail.com>
2023-04-24 16:08:53 +0330

    make_massage methode added

--- python_mail_system/email/email_manager.py
+++ python_mail_system/email/email_manager.py
@@ -1,11 +1,18 @@
 from email_model import Email
-from datastore.database_manager import DatabaseHandler
-from utils.database_connector import PostgreSQLDatabase
+from datastore.tables import get_emailes_db
 from exceptions import *
 
 
 class EmailManager:
-    email = Email
 
-    def make_massage(cls):
-        pass
+    email = Email
+    emailes_db = get_emailes_db()
+
+    @classmethod
+    def make_massage(cls, subject, body, email_id, send_id, recip_id, folder_id):
+        try:
+            email_instance = cls.email(subject, body, email_id, send_id, recip_id, folder_id)
+            emailes_db.insert(email_instance.dict_attributes())
+        except EmptyFieldError as err:
+            print(err)
+    
