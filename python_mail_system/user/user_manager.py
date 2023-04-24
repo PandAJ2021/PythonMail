e8f204085f2bcda05954a2e197c35cb637bb51cf
PandAJ2021 <pandaj200021@gmail.com>
2023-04-24 12:44:32 +0330

    all methodes updated to class methodes

--- python_mail_system/user/user_manager.py
+++ python_mail_system/user/user_manager.py
@@ -1,64 +1,58 @@
 from user.user_model import User
 from datastore.database_manager import DatabaseHandler
 from utils.database_connector import PostgreSQLDatabase
-from exceptions import *
 import hashlib
+from utils.tables import get_users_db
+from exceptions import *
 
-
-user_db_connector = PostgreSQLDatabase(
-    dbname='postgres', user='postgres', password='1234')
-
-users_db = DatabaseHandler('users', user_db_connector)
-
-users_db.create_table({
-    'id': 'bigserial primary key Not Null',
-    'name': 'varchar(50)',
-    'username': 'varchar(50)',
-    'salt': 'text Not Null',
-    'password': 'text Not null',
-    'logging': 'boolean default false'
-})
 
 
 class UserManager:
 
-    @staticmethod
-    def auth_username(username):
+    users_db = get_users_db()
+    user = User
+
+    @classmethod
+    def auth_username(cls, username):
         # stored users is a list of one member tuples.
-        stored_users = users_db.read('username', f"username = '{username}'")
+        stored_users = cls.users_db.read('username', f"username = '{username}'")
         return username in [t[0] for t in stored_users]
 
-    @staticmethod
-    def auth_pass(username, password):
-        salt, stored_pass = users_db.read(
+    @classmethod
+    def auth_pass(cls, username, password):
+        salt, stored_pass = cls.users_db.read(
             columns='salt, password', condition=f"username = '{username}'")[0]
         entered_pass = password + salt
         hashed_entered = hashlib.md5(entered_pass.encode()).hexdigest()
         return hashed_entered == stored_pass
 
-    @staticmethod
-    def register_user(full_name: str, username: str, password: str):
+    @classmethod
+    def register_user(cls, full_name: str, username: str, password: str):
         try:
-            user = User(full_name, username, password)
-            if UserManager.auth_username(username):
+            my_user = cls.user(full_name, username, password)
+            if cls.auth_username(username):
                 raise UserNameAlreadyExist
-            users_db.insert(user.dict_attribute())
+            cls.users_db.insert(my_user.dict_attribute())
         except (InvalidNameFormat, InvalidPassword, InvalidUsername, UserNameAlreadyExist) as err:
             print(err)
 
-    @staticmethod
-    def logging_user(username: str, password: str):
+    @classmethod
+    def logging_user(cls, username: str, password: str):
         try:
-            if not UserManager.auth_username(username) and UserManager.auth_pass(username, password):
+            if not cls.auth_username(username) and cls.auth_pass(username, password):
                 raise AuthenticationError
-            users_db.update({'logging': True}, f"username = '{username}'")
-            user_row = users_db.read(
+            cls.users_db.update({'logging': True}, f"username = '{username}'")
+            user_row = cls.users_db.read(
                 'id, name , username', f"username = '{username}'")[0]
-            user = User(user_row[1],user_row[2],"FakePass1",user_row[0] )
+            my_user = cls.user(user_row[1],user_row[2],"FakePass1",user_row[0] )
             #becuse I dont need passwd and can't use hash pass here
-            return user
-        except (InvalidPassword, InvalidUsername, AuthenticationError) as err:
+            return my_user
+        except AuthenticationError as err:
             print(err)
 
-    # @staticmethod
-    # def logout():
+    @classmethod
+    def get_id(username):
+        if not UserManager.auth_username(username):
+            raise UserNotFound
+        user_id = cls.users_db.read('id', f"username = '{username}'")[0][0]
+        return user_id
