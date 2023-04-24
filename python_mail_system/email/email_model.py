32b434bcb953d16f5be4860a516cc2f95ec0bc2b
PandAJ2021 <pandaj200021@gmail.com>
2023-04-24 16:07:59 +0330

    dict_attributes added

--- python_mail_system/email/email_model.py
+++ python_mail_system/email/email_model.py
@@ -3,11 +3,13 @@ from datetime import datetime
 
 class Email:
 
-    def __init__(self, subject: str, body: str, email_id=None, send_id=None, recip_id=None, status=None):
+    def __init__(self, subject: str, body: str, email_id=None,
+                 send_id=None, recip_id=None, folder_id=None, status=None):
 
         self.email_id = email_id
         self.send_id = send_id
         self.recip_id = recip_id
+        self.folder_id = folder_id
         self.subject = subject
         self.body = body
         self.time_stamp = datetime.now()
@@ -32,3 +34,13 @@ class Email:
         if not value:
             raise EmptyFieldError
         self._body = value
+
+    def dict_attributes(self):
+        return {
+            'subject': self.subject,
+            'body': self.body,
+            'sender_id': self.send_id,
+            'recipient_id': self.recip_id,
+            'folder_id': self.foder_id,
+            'time_stamp': self.time_stamp,
+        }
