19d9f6424f45115db742513763f403314cfb900a
PandAJ2021 <pandaj200021@gmail.com>
2023-04-29 22:45:01 +0330

    Manage class added, send_msg, sentbox,draft methodes added , inbox and routers updated

--- python_mail_system/main.py
+++ python_mail_system/main.py
@@ -3,40 +3,88 @@ from utils.database_connector import PostgreSQLDatabase
 from user.user_manager import UserManager
 from massage.email_manager import EmailManager
 from utils.user_interface import Menu , Menu_item
+from datetime import datetime
 
+class Manage:
+
+    user = UserManager
+    email = EmailManager
+    user_id = None
+
+    @classmethod
+    def register(cls):
+        name = input('Enter your full name : ')
+        username = input('Enter your username: ')
+        passwd = input('Enter your password: ')
+        cls.user.register_user(name , username , passwd)
+
+    @classmethod
+    def log_in(cls):
+        username = input('Enter your username: ')
+        passwd = input('Enter your password: ')   
+        cls.user_id = cls.user.logging_user(username, passwd)
+
+    @classmethod
+    def log_out(cls):
+        if cls.user_id :
+            user_id = cls.user.logout(cls.user_id)
+        user_id = None
     
-def register():
-    name = input('Enter your full name : ')
-    username = input('Enter your username: ')
-    passwd = input('Enter your password: ')
-    UserManager.register_user(name , username , passwd)
-    # print("You sign up successfully")
+    #Mj12345678
+    @classmethod
+    def send_msg(cls):
+        print('\n===== Sending massage =====\n')
+        subject = input("Enter the subject: ")
+        body = input("Enter your massage: ")
+        receiver_usrname = input("Enter receiver username: ")
+        recip_id = cls.user.get_id(receiver_usrname)
+        massage_id = cls.email.make_massage(subject, body, cls.user_id, recip_id)
+        ask = input('Do you want to send massage? (Y/n)')
+        if ask[0] in 'Nn':
+            print('massage saved in draft.')
+        else :
+            cls.email.send_massage(massage_id)
+            print('massage sent successfully.')
+        
+    @classmethod
+    def inbox(cls):
+        if cls.user_id :
+            a= cls.email.show_inbox(cls.user_id) 
+            for tup in a :
+                print(f"\nfrom {cls.user.get_username_from_id(tup[0])}",
+                f", 'subject': {tup[1]}, 'received in': {tup[3].ctime()}, 'read_state': {tup[4]}" ,
+                f", 'massage': (({tup[2]}))")
+            
+    @classmethod
+    def sentbox(cls):
+        if cls.user_id :
+            a= cls.email.show_sentbox(cls.user_id)
+            for tup in a :
+                print(f"\nto {cls.user.get_username_from_id(tup[0])}",
+                f", 'subject': {tup[1]}, 'send in': {tup[3].ctime()}" ,
+                f", 'massage': (({tup[2]}))")
 
-def log_in():
-    username = input('Enter your username: ')
-    passwd = input('Enter your password: ')   
-    global user_id
-    user_id = UserManager.logging_user(username, passwd)
+    @classmethod 
+    def draft(cls):
+        if cls.user_id :
+            a= cls.email.show_draft(cls.user_id)
+            for tup in a :
+                print(f"\nto {cls.user.get_username_from_id(tup[0])}",
+                f", 'subject': {tup[1]}, 'send in': {tup[3].ctime()}" ,
+                f", 'massage': (({tup[2]}))")
+        
 
-def inbox():
-    a= EmailManager.show_inbox(user_id)   
-    print(a)
 
-user_id = 1
-
-routers = Menu_item('Login or Register',condition =True ,  children = [
-    Menu_item('Sign up' ,condition= True , function=register),
-    Menu_item('Sign in' , condition= True,function=log_in) ,
-    Menu_item('User Panel' , condition= UserManager.user_status, children=[
-        Menu_item('Send massage'),
-        Menu_item('Sent box'),
-        Menu_item('inbox' , function= inbox ),
-        Menu_item('Draft'),
+routers = Menu_item('Login or Register', children = [
+    Menu_item('Sign up' ,function= Manage.register),
+    Menu_item('Sign in' ,function= Manage.log_in) ,
+    Menu_item('User Panel' , condition= UserManager.get_user_status, children=[
+        Menu_item('Send massage', function= Manage.send_msg),
+        Menu_item('Sent box', function= Manage.sentbox),
+        Menu_item('inbox' , function= Manage.inbox ),
+        Menu_item('Draft', function= Manage.draft),
         ]) , 
-    Menu_item('Logout')])
+    Menu_item('Logout',condition= UserManager.get_user_status , function= Manage.log_out)])
 
 
 Menu.run_menu(routers)
-# print(user_id)
-
-# log_in()
